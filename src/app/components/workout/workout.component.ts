7e7e94901390864c509f3ddb5a37fa7229029f90
Saging Con Yelo <96810918+BohemianRasberry@users.noreply.github.com>
2024-12-31 18:59:04 +0800

    on episode 15

---
+++ src/app/components/workout/workout.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { UserService } from '../../service/user.service';
+
+@Component({
+  selector: 'app-workout',
+  imports: [SharedModule],
+  templateUrl: './workout.component.html',
+  styleUrl: './workout.component.scss'
+})
+export class WorkoutComponent {
+  gridStyle = {
+    width: '100%',
+    textAlign: 'center'
+  }
+
+  workoutForm!: FormGroup;
+
+  listOfType: any[] = [
+    "Cardio",
+    "Stregth",
+    "Flexibility",
+    "HIIT",
+    "Pilates",
+    "Crossfit",
+    "Dance",
+    "Swimming",
+    "Cycling",
+    "Running",
+    "Walking",
+    "Boxing",
+    "Rowing",
+    "Stretching",
+    "Martial Arts",
+    "Gymnastics",
+    "Climbing",
+    "Plyometrics"
+  ];
+
+  constructor(
+      private fb: FormBuilder, 
+      private message: NzMessageService,
+      private userService: UserService
+    ) {}
+
+    ngOnInit(): void {
+        this.workoutForm = this.fb.group({
+          type: [null, [Validators.required]],
+          duration: [null, [Validators.required]],
+          caloriesBurnt: [null, [Validators.required]],
+          date: [null, [Validators.required]]
+        });
+    }
+
+    submitForm(): void {
+      if (this.workoutForm.valid) {
+        this.userService.postWorkout(this.workoutForm.value).subscribe(
+          res => {
+            this.message.success('Workout posted successfully', { nzDuration: 3000 });
+            this.workoutForm.reset();
+  
+          },
+          error => {
+            this.message.error('Error while posting Workout', { nzDuration: 3000 });
+          }
+        );
+      } else {
+        this.message.error('Please fill out the form correctly', { nzDuration: 3000 });
+      }
+    }
+}
